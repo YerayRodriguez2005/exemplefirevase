cecc3f91472ef5a524df9dc91e9406f1e81db094
Yeray Rodríguez Cabrera <yerayrodriguez@ceroca.cat>
2025-02-11 17:08:38 +0100

    Pagina Registro Inicial
    Componentes creados:
    TextField_Auth y Boton_Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exemplefirevase/paginas/pagina_resgistro.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: PaginaResgistro(),
+      
     );
   }
 }
---
+++ lib/paginas/pagina_resgistro.dart
@@ -0,0 +1,121 @@
+import 'package:exemplefirevase/Componentes/boton_auth.dart';
+import 'package:exemplefirevase/Componentes/textfield_auth.dart';
+import 'package:flutter/material.dart';
+
+class PaginaResgistro extends StatelessWidget {
+  const PaginaResgistro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController tecEmail = TextEditingController();
+    final TextEditingController tecPassword = TextEditingController();
+    final TextEditingController tecConfPassw = TextEditingController();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 183, 159),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Logo
+                const Icon(Icons.fireplace,
+                    size: 120, color: Color.fromARGB(255, 255, 240, 218)),
+                const SizedBox(
+                  height: 25,
+                ),
+                //Frase
+                const Text(
+                  "Crear una cuenta nueva",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 255, 240, 218),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+
+                //Texto divisor
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Divider(
+                              thickness: 1,
+                              color: Color.fromARGB(255, 255, 240, 218))),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 4),
+                        child: Text(
+                          "Registrate",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 240, 218)),
+                        ),
+                      ),
+                      Expanded(
+                          child: Divider(
+                              thickness: 1,
+                              color: Color.fromARGB(255, 255, 240, 218))),
+                    ],
+                  ),
+                ),
+                //TextField email
+                TextfieldAuth(
+                  controller: tecEmail,
+                  hintText: "Escribe tu email...",
+                  obscureText: false,
+                ),
+                //TextField password
+                TextfieldAuth(
+                  controller: tecConfPassw,
+                  obscureText: true,
+                  hintText: "Escoje una contraseña",
+                ),
+
+                //TextField confirm password
+                TextfieldAuth(
+                  controller: tecPassword,
+                  obscureText: true,
+                  hintText: "Reescribe la contraseña",
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+                //Boton no estas registrado?
+                 Padding(
+                  padding: const EdgeInsets.only(right: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        "Ya tienes una cuenta?",
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      GestureDetector(
+                        child: const Text(
+                          "Haz login",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 40, 71, 97)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                //Boton registrarse
+                BotonAuth()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
